d6101f42fc857fa07b7bfaa9a1d153a1a9cc17e7
niksolaz <solazzo.nicola@gmail.com>
2015-10-05 21:49:15 +0200

    add css

--- index.jsx
+++ index.jsx
@@ -1,16 +1,26 @@
 /** @jsx React.DOM */
 
-var MyComponent = React.createClass({
+var Counter = React.createClass({
+  incrementCount: function(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  getInitialState: function(){
+     return {
+       count: 0
+     }
+  },
   render: function(){
     return (
-      <h1>Hello , {this.props.name}</h1>
+      <div class="my-component">
+        <h1>Count: {this.state.count}</h1>
+        <button type="button" onClick={this.incrementCount}>Increment</button>
+      </div>
     );
   }
 });
 
-React.render(
-        <MyComponent name="Developers"/>,
-        document.getElementById('content')
-);
+React.render(<Counter/>, document.getElementById('content'));
 
-module.exports =  index;
+module.exports = index;
